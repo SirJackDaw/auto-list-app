7b3036535a52ca72dbe617a8b5bcfc3ab15ea7ce
Dmitry Zarubin <sirjackdaw@outlook.com>
2024-01-25 16:40:05 +0700

    config to cli and try to make request from cli

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,15 @@
-const message: string = 'Hello World';
+#!/usr/bin/env node
+import yargs from "yargs";
 
-console.log(message);
+const print5Moves = async (pockemonName: string) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pockemonName}`);
+    const pockemon: any = await response.json();
+    const moves = pockemon.moves.map(({ move } : any) => move.name).slice(0, 6);
+    console.log(moves)
+}
+
+const argv = await yargs(process.argv).argv;
+const pockemon: string = argv.pockemon as string;
+console.log(pockemon)
+
+print5Moves(pockemon)
